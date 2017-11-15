10cd3aa68b3d04d85401d67eb5b4bc3391381981
Stefan-Dator\Stefan <stefan85e@gmail.com>
2017-11-15 13:45:52 +0100

    TDD calc working

---
+++ calculator.c
@@ -0,0 +1,17 @@
+#include "calculator.h"
+float add(float num1, float num2)
+{
+    return num1 + num2;
+}
+float sub(float num1, float num2)
+{
+    return num1 - num2;
+}
+float mult(float num1, float num2)
+{
+    return num1 * num2;
+}
+float div(float num1, float num2)
+{
+    return num1 / num2;
+}
---
+++ calculator.h
@@ -0,0 +1,7 @@
+#include <stdint.h>
+#include "unity/src/unity.h"
+
+float add(float num1, float num2);
+float sub(float num1, float num2);
+float mult(float num1, float num2);
+float div(float num1, float num2);
